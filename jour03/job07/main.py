02a163fb9e599bf3f7acc212247e08ee260f4086
thomas-chardin <thomas.chardin@laplateforme.io>
2023-11-22 18:21:51 +0100

    job07 fait

---
+++ jour03/job07/main.py
@@ -0,0 +1,16 @@
+def tu_es(langage):
+    if langage == "JavaScript":
+        print("tu es développeur web")
+    elif langage == "python":
+        print("tu es un développeur IA")
+    elif langage == "java":
+        print("tu es un développeur logiciel")
+    elif langage == "reactjs":
+        print("tu es un developpeur mobile")
+    else:
+        print("un jour, je serai le meilleur développeur...")
+        
+tu_es("JavaScript")
+tu_es("reactjs")
+tu_es("python")
+tu_es("java")
