93a8fa3c590bea645fbf19a57d5723bc6592b4ea
thomas-chardin <thomas.chardin@laplateforme.io>
2023-11-23 19:05:47 +0100

    job12 fait

--- jour04/job12/main.py
+++ jour04/job12/main.py
@@ -1,11 +1,15 @@
-L = [8, 24, 27, 48, 2,16, 9, 102, 7, 84, 91]
-
-def trie(liste):
-    r=[]
-    while liste:
-        mini=min(liste)
-        liste.remove(mini)
-        r.append(mini)
-    return r
-     
-print(trie(L))
+def tri_a_bulles(liste):
+    n = 0
+    for tpm in liste:
+        n += 1
+    i = 0
+    while i < n:
+        j = 0
+        while j < n - i - 1:
+            if liste[j] > liste[j + 1]:
+                liste[j], liste[j + 1] = liste[j + 1], liste[j]
+            j += 1
+        i += 1
+test = [64, 34, 25, 12, 22, 11, 90]
+tri_a_bulles(test)
+print(test)
